2b814680b6813d5c9a34805a83dd7d263d55ffda
tangwenbiao <tangwenbiao@sm.vvip-u.com>
2019-10-28 19:39:15 +0800

    增加测试类

---
+++ test-cloud-server/test-cloud-server-facade/src/main/java/com/test/cloud/server/facade/ITest.java
@@ -0,0 +1,20 @@
+package com.test.cloud.server.facade;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+/**
+ * @author: TangFenQi
+ * @description:
+ * @date：2019/10/18 17:28
+ */
+@FeignClient(name = "provider")
+public interface ITest {
+
+  @RequestMapping(value = "/hello", method = RequestMethod.POST)
+  void test(@RequestBody String name);
+
+}
